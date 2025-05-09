ba8d25f2ffd7ec4b0d387f08f3eb469d286930a8
kopyl <oleggggg1997@gmail.com>
2025-05-09 11:13:54 +0300

    Make it possible to drag overlay image

--- place-image-on-video-2/SelectedVideo.swift
+++ place-image-on-video-2/SelectedVideo.swift
@@ -1,38 +1,111 @@
 import UIKit
 import AVKit
 
+func getVideoAspectRatio(from playerViewController: AVPlayerViewController) -> CGFloat? {
+    guard let asset = playerViewController.player?.currentItem?.asset else { return nil }
+    let tracks = asset.tracks(withMediaType: .video)
+    guard let track = tracks.first else { return nil }
+    
+    let size = track.naturalSize
+    return size.width / size.height
+}
+
+class DraggableImageView: UIImageView {
+    private var initialTouchPoint: CGPoint = .zero
+    private var playerViewController: AVPlayerViewController?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(playerViewController: AVPlayerViewController) {
+        super.init(frame: .zero)
+        isUserInteractionEnabled = true
+        self.playerViewController = playerViewController
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        initialTouchPoint = touch.location(in: self)
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first,
+              let superview = self.superview else { return }
+
+        let locationInSuperview = touch.location(in: superview)
+         self.center.y = locationInSuperview.y - initialTouchPoint.y + self.bounds.size.height / 2
+        
+        let bottomLimit = playerViewController?.view.frame.minY ?? 0
+        let topLimit = playerViewController?.view.frame.maxY ?? 0
+        if self.center.y < bottomLimit + self.bounds.size.height / 2 {
+            self.center.y = bottomLimit + self.bounds.size.height / 2
+        } else if self.center.y > topLimit - self.bounds.size.height / 2 {
+            self.center.y = topLimit - self.bounds.size.height / 2
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
+
 class SelectedVideoView: UIView {
     var pickImageTapped: (() -> Void)?
-    public let playerView: UIView
+    private var playerViewController: AVPlayerViewController
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    init(playerView: UIView) {
-        self.playerView = playerView
+    init(playerViewController: AVPlayerViewController) {
+        self.playerViewController = playerViewController
         super.init(frame: .zero)
         setupView()
     }
     
     private func setupView() {
-        playerView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(playerView)
-        NSLayoutConstraint.activate([
-            playerView.heightAnchor.constraint(equalToConstant: 500),
-            playerView.widthAnchor.constraint(equalTo: widthAnchor),
-            playerView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            playerView.centerYAnchor.constraint(equalTo: centerYAnchor),
-        ])
+        guard let videoAspectRatio = getVideoAspectRatio(from: playerViewController) else { return }
+        
+        playerViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(playerViewController.view)
         
         let pickImageButton = Button(title: "Pick Image to place on top of this video")
         pickImageButton.addTarget(self, action: #selector(pickImageTappedAction), for: .touchUpInside)
         addSubview(pickImageButton)
         pickImageButton.placeAtTheBottom(of: self)
+        
+        NSLayoutConstraint.activate([
+            playerViewController.view.centerXAnchor.constraint(equalTo: centerXAnchor),
+            playerViewController.view.centerYAnchor.constraint(equalTo: centerYAnchor),
+            playerViewController.view.widthAnchor.constraint(equalTo: widthAnchor, constant: -50),
+            playerViewController.view.heightAnchor.constraint(equalTo: playerViewController.view.widthAnchor, multiplier: 1 / videoAspectRatio),
+       ])
     }
     
     @objc private func pickImageTappedAction() {
         pickImageTapped?()
+    }
+    
+    public func addImage(image: URL) {
+        let imageView = DraggableImageView(playerViewController: playerViewController)
+        imageView.image = UIImage(contentsOfFile: image.path)
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(imageView)
+        
+        let imageRatio = imageView.intrinsicContentSize.height / imageView.intrinsicContentSize.width
+        
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalTo: playerViewController.view.widthAnchor),
+            imageView.centerXAnchor.constraint(equalTo: playerViewController.view.centerXAnchor),
+            imageView.bottomAnchor.constraint(equalTo: playerViewController.view.bottomAnchor),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: imageRatio),
+        ])
     }
 }
 
@@ -55,13 +128,14 @@ class SelectedVideoViewController: UIViewController {
     override func loadView() {
         guard let videoURL else { return }
         
-        selectedVideoView = SelectedVideoView(playerView: playerViewController.view)
+        playerViewController.player = AVPlayer(url: videoURL)
+        selectedVideoView = SelectedVideoView(playerViewController: playerViewController)
         selectedVideoView.pickImageTapped = { [weak self] in
             self?.imagePicker.presentMediaPicker(forType: .image)
         }
         view = selectedVideoView
         
-        playerViewController.player = AVPlayer(url: videoURL)
+        playerViewController.videoGravity = .resize
         playerViewController.player?.play()
         addChild(playerViewController)
     }
@@ -71,6 +145,8 @@ class SelectedVideoViewController: UIViewController {
         
         imagePicker.imagePicked = { [weak self] imageURL in
             print(self?.videoURL ?? "", imageURL)
+            self?.selectedVideoView.addImage(image: imageURL)
         }
+        
     }
 }
