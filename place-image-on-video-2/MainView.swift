e6020820f2cbfd9cdaa8997c9c62ba2eaadb4f7a
kopyl <oleggggg1997@gmail.com>
2025-05-09 04:48:09 +0300

    Improve file structure

---
+++ place-image-on-video-2/MainView.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class MainView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    private func setupView() {
+        backgroundColor = .white
+
+        let label = UILabel()
+        label.text = "Hello, world!"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(label)
+
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
+    }
+}
--- place-image-on-video-2/ViewController.swift
+++ place-image-on-video-2/ViewController.swift
@@ -1,36 +1,5 @@
 import UIKit
 
-class MainView: UIView {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = .red
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    init() {
-        super.init(frame: .zero)
-        setupView()
-    }
-    
-    private func setupView() {
-        backgroundColor = .white
-
-        let label = UILabel()
-        label.text = "Hello, world!"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        addSubview(label)
-
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: centerYAnchor),
-        ])
-    }
-}
-
 class ViewController: UIViewController {
     override func loadView() {
         view = MainView()
