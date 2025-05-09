307766ba5935f418b512d7b6366c92244f9f7ca2
kopyl <oleggggg1997@gmail.com>
2025-05-09 04:45:38 +0300

    Organize app

--- place-image-on-video-2/ViewController.swift
+++ place-image-on-video-2/ViewController.swift
@@ -1,22 +1,38 @@
 import UIKit
 
-class ViewController: UIViewController {
+class MainView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        view.backgroundColor = .white
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        setupView()
+    }
+    
+    private func setupView() {
+        backgroundColor = .white
 
         let label = UILabel()
         label.text = "Hello, world!"
-        label.font = .systemFont(ofSize: 24, weight: .bold)
-        label.textColor = .black
         label.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label)
+        
+        addSubview(label)
 
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor),
         ])
+    }
+}
+
+class ViewController: UIViewController {
+    override func loadView() {
+        view = MainView()
     }
 }
